f9d8000b4ea7846be1d3449fd07c0217bd9d1d4b
sajib sarkar <thebapi@gmail.com>
2016-12-01 00:05:22 +0600

    first commit

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('eServices', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
-
-})();
