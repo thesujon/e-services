82d18840bea8f975ea8775ad2d7b710a05ed2c47
sajib sarkar <thebapi@gmail.com>
2016-12-01 14:27:37 +0600

    service form has been added

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('serviceForm', { title: 'MyIMMs - e-Services' });
 });
 
 module.exports = router;
